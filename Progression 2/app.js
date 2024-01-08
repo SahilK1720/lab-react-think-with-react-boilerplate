185b2a42d749d9b6001db66398a659f038925166
Sahil Kharatmol <sahilkharatmol09@gmail.com>
2024-01-08 13:14:14 +0530

    done

--- Progression 2/app.js
+++ Progression 2/app.js
@@ -1,14 +1,13 @@
+//Classcomponent
+
 class ReactContainer extends React.Component{
-    // JSX tags
-    // JSX there should be one parent element
-      render(){
-          return (
-          <div>Hello! Welcome to Kalvium   
-             <div>This is babel</div>
-         </div> )
-      };  
-  }
+  render(){
+      return (
+      <div>Hello! Welcome to Kalvium   
+         <div>This is babel</div>
+     </div> )
+  };  
+}
 
 const container = document.getElementById('react-container');
-// Direclty pass the component to render
 ReactDOM.render(<ReactContainer/>,container);    
