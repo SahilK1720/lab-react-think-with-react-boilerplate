19beb7ca992180aa4be300a9a74ea691516e52ef
Sahil Kharatmol <sahilkharatmol09@gmail.com>
2024-01-18 11:57:59 +0530

    done

--- Progression 1/app.js
+++ Progression 1/app.js
@@ -12,7 +12,7 @@ const container = document.getElementById("react-container")
 
 //Class Component
 
-class ReactContainer extends ReactContainer.Component{
+class ReactContainer extends React.Component{
     render(){
         return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning!`,
         React.createElement(`div`,null, `Let's rock and roll`));
